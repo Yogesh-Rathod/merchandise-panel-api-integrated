6901617d2dc911a7604267f39409d43f4a405b02
Yogesh Rathod <yogesh.rathod@loylty.com>
2017-10-11 12:13:37 +0530

    Production Environment File Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,17 @@
 export const environment = {
-  production: true,
-  api_url: 'http://192.168.250.224:8080',
-  crm_url: 'http://192.168.250.222:9081/crm',
+  production: false,
+  apiUrl: 'http://192.168.250.224:8080',
+  crmUrl: 'http://192.168.250.222:9081/crm',
+  loginUrl: 'http://localhost:4300/',
+  usersUrl: 'http://192.168.250.222:8088',
+  customersAPIUrl: 'http://192.168.250.232:8310',
+  rbacUrl: 'http://192.168.250.222:8088/security',
+  masterApiUrl: '',
+  storeApiUrl: '',
+  appName: 'CRM',
+  domainName: 'localhost',
+  timeOut: 30000,
   crm: {
-    itemsPerPage: 5,
-  },
+    itemsPerPage: 10,
+  }
 };
