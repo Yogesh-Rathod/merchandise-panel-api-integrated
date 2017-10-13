639b9e051844372563b19f86cde5b432f52004b1
Yogesh Rathod <yogesh.rathod@loylty.com>
2017-10-13 14:14:27 +0530

    Detail page should open at top

--- src/app/pages/merchandise/merchandise.component.ts
+++ src/app/pages/merchandise/merchandise.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'merchandise',
-  template: `<router-outlet></router-outlet>`,
+  template: `<router-outlet (activate)="onActivate($event, outlet)" #outlet></router-outlet>`,
 })
 export class MerchandiseComponent {
   constructor() {}
+
+  onActivate(e, outlet): void {
+    window.scrollTo(0, 0);
+  }
 }
