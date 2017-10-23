61bbda61f9a4e05033a9baf1ce6702197fa8121f
Yogesh Rathod <yogesh.rathod@loylty.com>
2017-10-23 13:10:14 +0530

    Add Product Form Updated

--- src/app/pages/merchandise/products/add-product/add-product.component.ts
+++ src/app/pages/merchandise/products/add-product/add-product.component.ts
@@ -101,7 +101,8 @@ export class AddProductComponent implements OnInit {
           this.addProductForm.controls['MrpPrice'].setValue(product.MrpPrice);
           this.addProductForm.controls['oldPrice'].setValue(product.MrpPrice);
           this.addProductForm.controls['retailPrice'].setValue(product.retailPrice);
-          this.addProductForm.controls['stockQuantity'].setValue(product.stockQuantity);
+          this.addProductForm.controls['stockQuantity'].setValue(product.stockQuantity); 
+          this.addProductForm.controls['vendor'].setValue(product.vendor);
           // this.addProductForm.controls['pictureName'].setValue(product.picture[0].url);
           this.addProductForm.controls['pictureAlt'].setValue(product.picture[0].alt);
           this.addProductForm.controls['pictureTitle'].setValue(product.picture[0].title);
@@ -130,7 +131,10 @@ export class AddProductComponent implements OnInit {
       shortDescription: addProductForm.shortDescription,
       fullDescription: addProductForm.fullDescription,
       sku: addProductForm.sku,
-      price: addProductForm.price,
+      MrpPrice: addProductForm.MrpPrice,
+      oldPrice: addProductForm.oldPrice,
+      vendor: addProductForm.vendor,
+      retailPrice: addProductForm.retailPrice,
       stockQuantity: addProductForm.stockQuantity,
       productType: 'Simple',
       published: addProductForm.published,
