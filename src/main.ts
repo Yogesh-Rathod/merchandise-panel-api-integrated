e556dc3aa2f2421976cdfcd76064b7fd0ec3f1b0
Yogesh Rathod <yogesh.rathod@loylty.com>
2017-11-01 11:39:12 +0530

    hot module reload changes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+
+if (environment.production) {
+  enableProdMode();
+} else {
+  if(module['hot']) {
+    module['hot'].accept();
+  }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
